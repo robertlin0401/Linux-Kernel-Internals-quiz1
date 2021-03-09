b44bfe89e82d5938657184244e7aa8f2f928bcaf
robertlin0401 <robertlin0401@gmail.com>
2021-03-09 16:46:58 +0800

    Refactor: quicksort
    add quicksort.c

---
+++ quicksort.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+#include "quicksort.h"
+
+void recursive_quicksort(node_t **list)
+{
+    if (!*list)
+        return;
+
+    node_t *pivot = *list;
+    int value = pivot->value;
+    node_t *p = pivot->next;
+    pivot->next = NULL;
+
+    node_t *left = NULL, *right = NULL;
+    while (p) {
+        node_t *n = p;
+        p = p->next;
+        list_add_node_t(n->value > value ? &right : &left, n);
+    }
+
+    recursive_quicksort(&left);
+    recursive_quicksort(&right);
+
+    node_t *result = NULL;
+    list_concat(&result, left);
+    list_concat(&result, pivot);
+	list_concat(&result, right);
+    *list = result;
+}
+
