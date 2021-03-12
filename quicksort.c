566a1078866fd2fc71c3ed33c6667b968e1fa575
robertlin0401 <robertlin0401@gmail.com>
2021-03-13 05:15:46 +0800

    Revise: implement non-recursive quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,5 +1,12 @@
+#include <stdlib.h>
 #include <stddef.h>
 #include "quicksort.h"
+
+static int *list2arr(node_t **, int);
+
+static inline void SWAP(int *a, int *b) {
+	*a ^= *b; *b ^= *a; *a ^= *b;
+}
 
 void recursive_quicksort(node_t **list, int elements)
 {
@@ -30,5 +37,49 @@ void recursive_quicksort(node_t **list, int elements)
 
 void non_recursive_quicksort(node_t **list, int elements)
 {
-	
+	#define MAX_LEVELS 300
+
+	int *arr = list2arr(list, elements);
+	int pivot, begin[MAX_LEVELS], end[MAX_LEVELS], iter = 0, L, R;
+
+	begin[0] = 0;
+	end[0] = elements;
+	while (iter >= 0) {
+		L = begin[iter];
+		R = end[iter] - 1;
+		if (L < R) {
+			pivot = arr[L];
+			while (L < R) {
+				while (arr[R] >= pivot && L < R) R--; if (L < R) arr[L++] = arr[R];
+				while (arr[L] <= pivot && L < R) L++; if (L < R) arr[R--] = arr[L];
+			}
+			arr[L] = pivot;
+			begin[iter+1] = L + 1;
+			end[iter+1] = end[iter];
+			end[iter++] = L;
+			if (end[iter]-begin[iter] > end[iter-1]-begin[iter-1]) {
+				SWAP(&begin[iter], &begin[iter-1]);
+				SWAP(&end[iter], &end[iter-1]);
+			}
+		} else {
+			iter--;
+		}
+	}
+
+	for (iter = 0; iter < elements; ++iter) {
+		(*list)->value = arr[iter];
+		list = &((*list)->next);
+	}
+	free(arr);
 }
+
+static int *list2arr(node_t **list, int elements)
+{
+	int *arr = (int *)malloc(elements * sizeof(int));
+	for (int iter = 0; iter < elements; ++iter) {
+		arr[iter] = (*list)->value;
+		list = &((*list)->next);
+	}
+	return arr;
+}
+
