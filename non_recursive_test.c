8b7e8187520cea0237801e520426fe1ec663f79e
robertlin0401 <robertlin0401@gmail.com>
2021-03-13 02:00:20 +0800

    Revise: invoke non-recursive quicksort
    add non_recursive_test.c, revised from optimized_random_test.c

---
+++ non_recursive_test.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/random.h>
+#include "list.h"
+#include "quicksort.h"
+
+void (*quicksort)(node_t **) = non_recursive_quicksort;
+
+int main(int argc, char **argv) {
+    size_t count = 20;
+	int *seed = (int *)malloc(sizeof(int));
+	int result = getrandom(seed, sizeof(int), GRND_RANDOM | GRND_NONBLOCK);
+	if (result == -1) *seed = result;
+	srandom(*seed);
+
+    node_t *list = NULL;
+    while (count--)
+        list = list_make_node_t(list, random() % 1024);
+
+    list_display(list);
+    (*quicksort)(&list);
+    list_display(list);
+
+	free(seed);
+    if (!list_is_ordered(list))
+        return EXIT_FAILURE;
+
+    list_free(&list);
+    return EXIT_SUCCESS;
+}
+
